a7f60dbbd26c100a5b8ee910f306e1cf392d216b
Shmitkin <mitkinoleg@gmail.com>
2019-05-22 17:19:44 +0300

    Минификация html и js

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,145 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var less = require("gulp-less");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var csso = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sourcemap = require('gulp-sourcemaps');
+var less = require('gulp-less');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
+var csso = require('gulp-csso');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
+var svgstore = require('gulp-svgstore');
+var posthtml = require('gulp-posthtml');
+var include = require('posthtml-include');
+var del = require('del');
+var htmlmin = require('gulp-htmlmin');
+var jsmin = require('gulp-uglify');
 
-gulp.task("css", function () {
-  return gulp.src("source/less/style.less")
+var css = {
+  build: function () {
+    return gulp.src('source/less/style.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.stream());
+  },
+
+  dev: function () {
+    return gulp.src('source/less/style.less')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('build/css'))
     .pipe(server.stream());
-});
+  }
+};
 
-gulp.task("server", function () {
+var js = {
+  build: function () {
+    return gulp.src('source/js/*.js')
+    .pipe(jsmin())
+    .pipe(gulp.dest('build/js'));
+  },
+
+  dev: function () {
+    return gulp.src(['source/js/**'], {base: 'source'})
+    .pipe(gulp.dest('build'));
+  }
+};
+
+gulp.task('js:build', js.build);
+
+gulp.task('js:dev', js.dev);
+
+gulp.task('css:build', css.build);
+
+gulp.task('css:dev', css.dev);
+
+gulp.task('server', function () {
   server.init({
-    server: "build/",
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/img/logo-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
+  gulp.watch('source/less/**/*.less', gulp.series('css:dev'));
+  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
+  gulp.watch('source/img/logo-*.svg', gulp.series('sprite', 'html', 'refresh'));
+  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
+  gulp.watch('source/js/*.js', gulp.series('js:dev', 'refresh'));
 });
 
-gulp.task("refresh", function (done) {
+gulp.task('refresh', function (done) {
   server.reload();
   done();
 });
 
-gulp.task("images", function () {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
+gulp.task('images', function () {
+  return gulp.src('source/img/**/*.{png,jpg,svg}')
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest('source/img'));
 });
 
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
+gulp.task('webp', function () {
+  return gulp.src('source/img/**/*.{png,jpg}')
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest('source/img'));
 });
 
-gulp.task("sprite", function () {
+gulp.task('sprite', function () {
   return gulp.src([
-      "source/img/icon-*.svg",
-      "source/img/logo-*.svg"
-    ])
+    'source/img/icon-*.svg',
+    'source/img/logo-*.svg'
+  ])
     .pipe(svgstore({
       inlineSvg: true
     }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
 });
 
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
+gulp.task('html', function () {
+  return gulp.src('source/*.html')
     .pipe(posthtml([
       include()
     ]))
-    .pipe(gulp.dest("build"));
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'));
 });
 
-gulp.task("copy", function () {
+gulp.task('copy', function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/js/**",
-    "source/*.ico",
-    "source/img/**"
+    'source/fonts/**/*.{woff,woff2}',
+    'source/*.ico',
+    'source/img/**'
   ], {
-    base:"source"
-    })
-    .pipe(gulp.dest("build"));
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'));
 });
 
-gulp.task("js", function () {
-  return gulp.src([
-    "source/js/**",
-  ], {
-    base:"source"
-    })
-    .pipe(gulp.dest("build"));
+gulp.task('clean', function () {
+  return del('build');
 });
 
-gulp.task("clean", function () {
-  return del("build");
-});
-
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
-gulp.task("start", gulp.series("build", "server"));
+gulp.task('build', gulp.series('clean', 'copy', 'css:build', 'js:build', 'sprite', 'html'));
+gulp.task('start', gulp.series('clean', 'copy', 'css:dev', 'js:dev', 'sprite', 'html', 'server'));
